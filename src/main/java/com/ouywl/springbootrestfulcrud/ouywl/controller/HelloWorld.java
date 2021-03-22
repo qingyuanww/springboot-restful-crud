6c37356b4783ae0fb8e313362141571dd0ce5939
24106 <2410667597@qq.com>
2021-03-22 18:03:10 +0800

    导入模板引擎，版本要对！！

--- src/main/java/com/ouywl/springbootrestfulcrud/ouywl/controller/HelloWorld.java
+++ src/main/java/com/ouywl/springbootrestfulcrud/ouywl/controller/HelloWorld.java
@@ -1,8 +1,12 @@
 package com.ouywl.springbootrestfulcrud.ouywl.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Map;
 
 /**
  * @program: springboot-restful-crud
@@ -10,12 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: ouyangwl
  * @create: 2021-03-22 16:55
  **/
-@RestController
+@Controller
 public class HelloWorld {
 
     @ResponseBody
     @RequestMapping("/hello-crud")
     public String t1(){
         return "Hello,World-springboot-crud!";
+    }
+
+    //查出用户数据，在页面展示
+    @RequestMapping("/success")
+    public String success(){
+//        map.put("hello","<h1>你好</h1>");
+//        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        return "success.html";
     }
 }
